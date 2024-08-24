39ed3de6931524d28cc5cc11541b0e1e6fd30f8d
lilianetsukamoto <133925337+lilianetsukamoto@users.noreply.github.com>
2024-08-24 18:05:44 -0300

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -33,6 +33,7 @@ HTTP_204_NO_CONTENT = 204
 @given('the following products')
 def step_impl(context):
     """ Delete all Products and load new ones """
+    
     #
     # List all of the products and delete them one by one
     #
@@ -46,13 +47,13 @@ def step_impl(context):
     #
     # load the database with new products
     #
-        for row in context.table:
-            payload = {
-                "name": row['name'],
-                "description": row['description'],
-                "price": row['price'],
-                "available": row['available'] in ['True', 'true', '1'],
-                "category": row['category']
-            }
-            context.resp = requests.post(rest_endpoint, json=payload)
-            assert context.resp.status_code == HTTP_201_CREATED
+    for row in context.table:
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
