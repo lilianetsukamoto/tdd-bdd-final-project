d30e1cd1edf3e6e6e4424d3b09415b80daf6a4ee
lilianetsukamoto <133925337+lilianetsukamoto@users.noreply.github.com>
2024-08-24 19:45:12 -0300

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -157,7 +157,7 @@ def step_impl(context, name):
 @then('I should not see "{name}" in the results')
 def step_impl(context, name):
     element = context.driver.find_element_by_id('search_results')
-    assert(name not in element.text)
+    assert name not in element.text
 
 ##################################################################
 # Verify that a specific message is present
